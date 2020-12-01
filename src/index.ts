ad41b7c07c6e194f825d934fc6c1a8d33de53b50
Finley Williamson V <finley.williamson@gmail.com>
2020-12-01 14:56:47 -0500

    Revert "Finalize boilerplate code for team use"
    This reverts commit 1ff55521acb1d40495ef1056c8e16bd6a071f29b.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs'
 
-alert('pingus');
+alert('pingus')
 
 const observable = Observable.create((observer:any) => {
-  observer.next('Hello World!');
-  observer.next('Hello Again!');
-  observer.complete();
-  observer.next('Bye');
-});
+  observer.next('Hello World!')
+  observer.next('Hello Again!')
+  observer.complete()
+  observer.next('Bye')
+})
 observable.subscribe(
   (x:any) => logItem(x),
   (error: any) => logItem(`Error: ${error}`),
   () => logItem('Completed'),
-);
+)
 function logItem(val:any) {
-  const node = document.createElement('li');
-  const textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.getElementById('list').appendChild(node);
+  const node = document.createElement('li')
+  const textnode = document.createTextNode(val)
+  node.appendChild(textnode)
+  document.getElementById('list').appendChild(node)
 }
