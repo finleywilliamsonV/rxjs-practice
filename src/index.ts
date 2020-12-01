4f31e67a07031acb70d0d5645543e9e49740723b
Finley Williamson V <finley.williamson@gmail.com>
2020-12-01 15:05:25 -0500

    Fix indent error

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { Observable } from 'rxjs'
 
 alert('pingus')
 
-const observable = Observable.create((observer:any) => {
-  observer.next('Hello World!')
-  observer.next('Hello Again!')
-  observer.complete()
-  observer.next('Bye')
+const observable = Observable.create((observer: any) => {
+    observer.next('Hello World!')
+    observer.next('Hello Again!')
+    observer.complete()
+    observer.next('Bye')
 })
 observable.subscribe(
-  (x:any) => logItem(x),
-  (error: any) => logItem(`Error: ${error}`),
-  () => logItem('Completed'),
+    (x: any) => logItem(x),
+    (error: any) => logItem(`Error: ${ error }`),
+    () => logItem('Completed'),
 )
-function logItem(val:any) {
-  const node = document.createElement('li')
-  const textnode = document.createTextNode(val)
-  node.appendChild(textnode)
-  document.getElementById('list').appendChild(node)
+function logItem(val: any) {
+    const node = document.createElement('li')
+    const textnode = document.createTextNode(val)
+    node.appendChild(textnode)
+    document.getElementById('list').appendChild(node)
 }
